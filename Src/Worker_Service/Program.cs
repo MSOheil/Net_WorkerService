dc892dc77b7dad52a0daa61005e7776a212c5bf3
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-13 13:02:45 +0330

    Initial commit

---
+++ Src/Worker_Service/Program.cs
@@ -0,0 +1,10 @@
+using Worker_Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();

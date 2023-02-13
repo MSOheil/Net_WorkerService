585ea034b57b2c4c0b33822ba759263768ab5481
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-13 14:31:35 +0330

    Added serilog and tested it

--- Src/Worker_Service/Program.cs
+++ Src/Worker_Service/Program.cs
@@ -1,10 +1,36 @@
+using Serilog;
+using Serilog.Events;
 using Worker_Service;
+
+
+Log.Logger = new LoggerConfiguration()
+       .MinimumLevel.Debug()
+       .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+       .Enrich.FromLogContext()
+       .WriteTo.File(Directory.GetCurrentDirectory() + @"Logfile.txt")
+       .CreateLogger();
+try
+{
+
+    Log.Information("App Strating up");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, ex.Message);
+
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
     })
+    .UseSerilog()
     .Build();
 
 host.Run();
